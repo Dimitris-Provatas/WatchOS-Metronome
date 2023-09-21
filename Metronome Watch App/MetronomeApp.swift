93dd7cdfeeadecacc393315e83501b1433e5c254
Dimitris Provatas <james.provatas@gmail.com>
2023-09-21 12:35:24 +0300

    Initial Commit

---
+++ Metronome Watch App/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome Watch App
+//
+//  Created by Dimitris Provatas on 21/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Metronome_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
